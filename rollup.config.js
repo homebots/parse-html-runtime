c77bf377fa293a59531e4cd02e37da51f5432c89
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-08-09 20:23:36 +0000

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,14 @@
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   external: [],
   output: {
-    dir: ".",
-    format: "es",
+    dir: '.',
+    format: 'es',
   },
-  plugins: [
-    typescript({ module: "esnext" }),
-    resolve(),
-    commonjs({ extensions: [".js", ".ts"] }),
-  ],
+  plugins: [typescript({ module: 'esnext' }), resolve(), commonjs({ extensions: ['.js', '.ts'] }), terser()],
 };
