62ed4e0032781189cae4629de310175cd67a291c
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-08-11 20:41:31 +0200

    fix: update exported symbols

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { normalize } from './normalize';
 export { serialize } from './serialize';
-export { materialize, createComment, createElement, createTextNode } from './materialize';
+export { materialize, createComment, createElement, createTextNode, setAttribute, setProperty } from './materialize';
 export type { Visitor } from './materialize';
 export type { ParserNode, ParserAttribute, CommentNode, TextNode, ElementNode, PackedAttributes, PackedChildNode, PackedCommentNode, PackedDocumentNode, PackedElementNode, PackedTextNode } from '@homebots/parse-html';
 export { pack, unpack } from '@homebots/parse-html';
